a6b86d96e40ba5f39ccd8bb3fbdfa1c74a554892
Tomasz Szymula <tehshy@gmail.com>
2015-01-20 16:47:55 +0100

    graham assignment data

--- asd-agh/src/main/scala/pl/pej/asd/geometric/Graham.scala
+++ asd-agh/src/main/scala/pl/pej/asd/geometric/Graham.scala
@@ -3,46 +3,25 @@ package pl.pej.asd.geometric
 import math.atan2
 
 object Graham {
-
   private type Points = List[Point]
 
   def scan(points: Points): Points = {
 
     val min: Point = points.minBy(_.y)
+    val sorted: Points = points.sortBy(point => atan2(point.y - min.y, point.x - min.x))
 
-    val sorted: Points = points.sortBy(point => atan2(point.y - min.y, point.x - min.x))//.foldLeft(List.empty[Point])(addToHull)
-
-    def newBorder(newPoint: Point, border: Points): Points = {
-
-      border match {
-        case Nil => newPoint::Nil
-
-        case a :: Nil => newPoint :: a :: Nil
-
-        case b :: a :: Nil => newPoint :: b :: a :: Nil
-
+    def newBorder(newPoint: Point, border: Points): Points = border match {
+        case list if list.size < 3 => newPoint :: list
         case c :: b :: tail =>
           if(PointsUtil.det(c, newPoint, b) <= 0) {
-            println(s"leaving out $c")
             newBorder(newPoint, b::tail)
           } else {
             newPoint :: border
           }
       }
+
+    sorted.foldLeft(List[Point]()) { case (acc, next) =>
+        newBorder(next, acc)
     }
-
-    def iterate(left: Points, acc: Points): Points = left match {
-
-      case next :: tail =>
-
-        val border = newBorder(next, acc)
-        println(border)
-
-        iterate(tail, border)
-      case Nil => acc
-    }
-
-
-    iterate(sorted, Nil)
   }
 }
